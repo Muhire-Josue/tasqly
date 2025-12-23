7fe2f1f7694a1e15a86f81133530b7eceb8b2d38
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-22 21:28:13 -0500

    Add SignUp screen and update splash screen navigation logic

--- mobile/src/screens/splash/index.tsx
+++ mobile/src/screens/splash/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
 import styles from "./style";
 import { FontAwesome5 } from "@expo/vector-icons";
 import Spinner from "../../components/common/Spinner";
+import { useNavigateTo } from "../../navigation/useNavigateTo";
 
 const Index: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const navigateTo = useNavigateTo();
+  setTimeout(() => {
+    navigateTo("signup");
+    setLoading(false);
+  }, 2000);
   return (
     <View style={styles.container}>
       <Text style={styles.brand}>
@@ -12,7 +20,7 @@ const Index: React.FC = () => {
         sqly
       </Text>
       <Text style={styles.subtitle}>Simplify tasks. Amplify life.</Text>
-      <Spinner />
+      {loading && <Spinner />}
     </View>
   );
 };
