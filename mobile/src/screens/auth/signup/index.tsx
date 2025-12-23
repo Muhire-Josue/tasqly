6cfada8c67e2efabb01e3c4fd0191bec5e2c7286
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-23 12:47:52 -0500

    - Add TextInput component for name entry in SignUp screen and update styles

--- mobile/src/screens/auth/signup/index.tsx
+++ mobile/src/screens/auth/signup/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from "react-native";
 import styles from "./styles";
+import { TextInput } from "react-native-paper";
 
 const SignUp: React.FC = () => {
   return (
@@ -8,6 +9,22 @@ const SignUp: React.FC = () => {
       <Text style={styles.subtitle}>
         Fill your information bellow or register with your social account.
       </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.label}>Name</Text>
+        <TextInput
+          // value={value}
+          // onChangeText={onChangeText}
+          placeholder="Ex: John Doe"
+          mode="outlined"
+          outlineColor="#CCC9C9"
+          activeOutlineColor="#CCC9C9"
+          style={styles.input}
+          contentStyle={styles.inputContent}
+          underlineColor="transparent"
+          activeUnderlineColor="transparent"
+          left={<TextInput.Icon icon="account" />}
+        />
+      </View>
     </View>
   );
 };
--- mobile/src/screens/auth/signup/styles.ts
+++ mobile/src/screens/auth/signup/styles.ts
@@ -1,20 +1,37 @@
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
+    marginTop: "10%",
     flex: 1,
-    alignItems: "center",
-    paddingTop: "20%",
-    paddingHorizontal: "7%",
+    paddingTop: 40,
+    paddingHorizontal: 24,
   },
   title: {
     fontSize: 24,
     fontWeight: "600",
     marginBottom: 10,
+    textAlign: "center",
   },
   subtitle: {
     fontSize: 16,
     color: "#666",
     marginBottom: 20,
+  },
+  wrapper: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 6,
+  },
+  input: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 15,
+  },
+  inputContent: {
+    paddingVertical: 6,
+    height: 48,
   },
 });
 
