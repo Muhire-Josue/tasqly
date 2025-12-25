a37d57226a8bf5ff48dcc799a371488cf3536888
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-25 18:02:25 -0500

    - adds the avatar image

--- mobile/src/screens/tasks/list/index.tsx
+++ mobile/src/screens/tasks/list/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import styles from "./styles";
+import ben from "../../../assets/ben.jpg";
 
 const TaskList: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Task List Screen</Text>
+      <View style={styles.headerRow}>
+        <Image source={ben} style={styles.avatar} />
+
+        <Text style={styles.greetingText}>Hi John</Text>
+      </View>
     </View>
   );
 };
--- mobile/src/screens/tasks/list/styles.ts
+++ mobile/src/screens/tasks/list/styles.ts
@@ -7,6 +7,23 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingHorizontal: 24,
   },
+  headerRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+    marginBottom: 20,
+  },
+
+  avatar: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+
+  greetingText: {
+    fontSize: 22,
+    color: "#000",
+  },
 });
 
 export default styles;
