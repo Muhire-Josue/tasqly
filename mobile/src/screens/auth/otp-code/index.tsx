4f3ebf040a40a3697371b88fd98b90b2edf14447
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-25 13:27:26 -0500

    - Adds empty otp component

---
+++ mobile/src/screens/auth/otp-code/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const OtpCode: React.FC = () => {
+    return (
+        <View>
+            <Text>OTP Code Screen</Text>
+        </View>
+    )
+};
+
+export default OtpCode;
