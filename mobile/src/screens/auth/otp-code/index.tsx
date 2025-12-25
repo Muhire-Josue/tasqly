c0562809adf99cdf271e2439546071b8e548d41b
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-25 13:28:52 -0500

    - fix formatting

--- mobile/src/screens/auth/otp-code/index.tsx
+++ mobile/src/screens/auth/otp-code/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const OtpCode: React.FC = () => {
-    return (
-        <View>
-            <Text>OTP Code Screen</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>OTP Code Screen</Text>
+    </View>
+  );
 };
 
 export default OtpCode;
