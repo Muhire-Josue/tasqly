6991039281a2ceba7ca542057e58ea61ac9cb97f
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-25 14:25:07 -0500

    - adds title and subtles

--- mobile/src/screens/auth/otp-code/index.tsx
+++ mobile/src/screens/auth/otp-code/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./styles";
 
 const OtpCode: React.FC = () => {
   return (
-    <View>
-      <Text>OTP Code Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Verify Code</Text>
+      <Text style={styles.subtitle}>Enter the code sent to your email</Text>
+      <Text style={styles.emailTitle}>email@example.com</Text>
     </View>
   );
 };
---
+++ mobile/src/screens/auth/otp-code/styles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from "react-native";
+import { PRIMARY_COLOR } from "../../../theme/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: "20%",
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  emailTitle: {
+    fontSize: 16,
+    color: PRIMARY_COLOR,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  otpWrapper: {
+    marginTop: 30,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+  },
+
+  otpInput: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 14,
+    borderWidth: 1.5,
+    borderColor: "#D6D6D6",
+    fontSize: 22,
+    fontWeight: "600",
+    color: "#333",
+  },
+});
+
+export default styles;
