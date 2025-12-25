933a77c98c93c94df7237c94bc745790c79a0ad7
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-24 22:04:36 -0500

    - Empty skelton

---
+++ mobile/src/screens/auth/reset-password/indext.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+const ResetPassword: React.FC = () => {
+  return (
+    <View>
+      <Text>Reset Password Screen</Text>
+    </View>
+  );
+};
+
+export default ResetPassword;
