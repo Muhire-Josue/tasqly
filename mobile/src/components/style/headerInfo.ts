13056b8399c0aba052b8eca499ab1904b5cd99d8
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-25 20:15:53 -0500

    - refactor: move greeting & home header into separate reusable component

---
+++ mobile/src/components/style/headerInfo.ts
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  headerRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 18,
+  },
+
+  userRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+
+  avatar: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+
+  greetingText: {
+    fontSize: 22,
+    color: "#000",
+  },
+
+  addButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  addButtonPressed: {
+    opacity: 0.6,
+  },
+
+  homeRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+
+  homeText: {
+    fontSize: 20,
+    color: "#000",
+    fontWeight: "500",
+  },
+});
+
+export default styles;
--- mobile/src/screens/tasks/list/index.tsx
+++ mobile/src/screens/tasks/list/index.tsx
@@ -1,33 +1,13 @@
 import React from "react";
-import FontAwesome from "@expo/vector-icons/build/FontAwesome";
-import Ionicons from "@expo/vector-icons/build/Ionicons";
-import { View, Text, Image, Pressable } from "react-native";
+import { View } from "react-native";
 import styles from "./styles";
-import ben from "../../../assets/ben.jpg";
 import Stats from "../../../components/common/Stats";
+import HeaderInfo from "../../../components/common/HeaderInfo";
 
 const TaskList: React.FC = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.headerRow}>
-        <View style={styles.userRow}>
-          <Image source={ben} style={styles.avatar} />
-          <Text style={styles.greetingText}>Hi John</Text>
-        </View>
-
-        <Pressable
-          style={({ pressed }) => [
-            styles.addButton,
-            pressed && styles.addButtonPressed,
-          ]}
-        >
-          <Ionicons name="add" size={36} color="#000" />
-        </Pressable>
-      </View>
-      <View style={styles.homeRow}>
-        <FontAwesome name="home" size={30} color="#000" />
-        <Text style={styles.homeText}>The Smith&apos;s Home</Text>
-      </View>
+      <HeaderInfo />
       <Stats />
     </View>
   );
