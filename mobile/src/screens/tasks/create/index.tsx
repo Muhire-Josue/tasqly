6f1dfb3683dbe845b02ba6fc58bde38250becf44
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2025-12-30 14:40:57 -0500

    - adds screen skeleton

---
+++ mobile/src/screens/tasks/create/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CreateTaskScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Create Task Screen</Text>
+    </View>
+  );
+};
+
+export default CreateTaskScreen;
