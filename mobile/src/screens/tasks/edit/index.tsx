aaf91153d9997fdc05e9e6e681d46af264dc3fdc
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-02 19:36:16 -0500

    - Add skeleton

---
+++ mobile/src/screens/tasks/edit/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const EditTask: React.FC = () => {
+  return (
+    <View>
+      <Text> Edit Task</Text>
+    </View>
+  );
+};
+
+export default EditTask;
