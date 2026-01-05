44edc1e14a50e5089ea82866eebc11b3c9d1e91e
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-05 11:48:34 -0500

    - create skeleton

---
+++ mobile/src/screens/repairs/list/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RepairList: React.FC = () => {
+  return (
+    <View>
+      <Text>Repair List</Text>
+    </View>
+  );
+};
+
+export default RepairList;
