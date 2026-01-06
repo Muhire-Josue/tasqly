448e06f3a9ea354f3585eeeea29851a66b3230b4
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-06 10:50:25 -0500

    - adds skeleton

---
+++ mobile/src/screens/notifications/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Notification: React.FC = () => {
+  return (
+    <View>
+      <Text>Notification Center</Text>
+    </View>
+  );
+};
+
+export default Notification;
