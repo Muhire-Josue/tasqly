e2c3e01196f9e88c7f5a1061e84d7e3c9e552d12
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-06 21:23:45 -0500

    - add no content styles

--- mobile/src/screens/notifications/no-content/style.ts
+++ mobile/src/screens/notifications/no-content/style.ts
@@ -1,5 +1,34 @@
 import { StyleSheet } from "react-native";
+import { PRIMARY_COLOR_BLUE } from "../../../theme/colors";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 80,
+  },
+
+  iconCircle: {
+    width: 210,
+    height: 210,
+    borderRadius: 105,
+    backgroundColor: PRIMARY_COLOR_BLUE,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 22,
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "500",
+    color: "#111",
+    letterSpacing: 0.2,
+  },
+
+  emoji: {
+    fontSize: 28,
+  },
+});
 
 export default styles;
