f162467b56fe6e6f1c518c05d84f4c267537dd5c
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-07 14:38:29 -0500

    - adds redirection from the profile screen

--- mobile/src/screens/household/style.ts
+++ mobile/src/screens/household/style.ts
@@ -1,5 +1,52 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#F7FAFB",
+  },
+  container: {
+    flex: 1,
+  },
+
+  imageWrapper: {
+    width: "100%",
+    height: 220,
+    position: "relative",
+    backgroundColor: "#E5E7EB",
+  },
+
+  houseImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+
+  editBadge: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    width: 42,
+    height: 42,
+    borderRadius: 21,
+    backgroundColor: "#111",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  headerRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    paddingHorizontal: 24,
+    paddingTop: 18,
+  },
+
+  headerTitle: {
+    fontSize: 28,
+    fontWeight: "500",
+    color: "#111",
+  },
+});
 
 export default styles;
