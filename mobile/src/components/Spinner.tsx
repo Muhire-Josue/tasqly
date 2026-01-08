bd9aa1fb7ec2e8b3ca7f79ee7be47e89ef29bf68
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-08 12:11:03 -0500

    - refactor spinner component

--- mobile/src/components/Spinner.tsx
+++ mobile/src/components/Spinner.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, ActivityIndicator } from "react-native";
+import styles from "./style/spinner";
 
 const Spinner: React.FC = () => {
   return (
-    <View>
-      <ActivityIndicator size="large" color="#fffff" />
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#FFFFFF" />
     </View>
   );
 };
