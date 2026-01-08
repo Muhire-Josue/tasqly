eb15dc3b15c2af4950675a484dd41f36682effac
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-08 12:07:51 -0500

    - add spinner styling

---
+++ mobile/src/components/style/spinner.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
