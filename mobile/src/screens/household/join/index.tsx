586a8d631acf2af1223cd3b1f9f16ff023acf686
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-08 21:15:21 -0500

    - no content skeleton

---
+++ mobile/src/screens/household/join/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const JoinHousehold: React.FC = () => {
+  return (
+    <View>
+      <Text>Join Household</Text>
+    </View>
+  );
+};
+
+export default JoinHousehold;
