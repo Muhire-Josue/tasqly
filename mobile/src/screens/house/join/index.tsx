ebdb15dde00e1b414508f9d6fe1318b96b6432af
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-09 18:30:22 -0500

    - adds join house skeleton

---
+++ mobile/src/screens/house/join/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const JoinHouse: React.FC = () => {
+  return (
+    <View>
+      <Text>Join House</Text>
+    </View>
+  );
+};
+
+export default JoinHouse;
