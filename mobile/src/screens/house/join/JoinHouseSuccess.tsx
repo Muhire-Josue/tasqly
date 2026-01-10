1e8451a5372b164022f75988c39284f6693e6ee7
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-01-09 20:46:47 -0500

    - adds join house success component (Skeleton)

---
+++ mobile/src/screens/house/join/JoinHouseSuccess.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+const JoinHouseSuccess: React.FC = () => {
+  return (
+    <View>
+      <Text>Join house success</Text>
+    </View>
+  );
+};
+
+export default JoinHouseSuccess;
