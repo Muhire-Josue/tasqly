d01d3943e38f1039e0d265c9d94ff9bd413e90fd
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-06-03 13:49:16 -0400

    add Terraform configuration files including VPC resource and variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   description = "AWS region"
   default     = "ca-central-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the Tasqly VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
