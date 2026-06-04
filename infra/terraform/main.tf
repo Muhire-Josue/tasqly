1f2f72d2d3e25abf7920f7f6cac2112c7d54c56a
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-06-04 14:39:22 -0400

    add internet gateway and public route table resources in Terraform configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -46,3 +46,35 @@ resource "aws_subnet" "private" {
     }
   )
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-igw"
+    }
+  )
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-public-rt"
+    }
+  )
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
