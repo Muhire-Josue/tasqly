2a211a186a20d202cf69f68071d71be0ac2a8dc9
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-06-05 11:46:04 -0400

    add private route table and association resources in Terraform configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -78,3 +78,21 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-private-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
