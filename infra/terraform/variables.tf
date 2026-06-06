1c65b9f1488d8e1710cb9efeec9bbeea21509ed2
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-06-06 15:48:43 -0400

    add RDS PostgreSQL database and subnet group resources with outputs

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -33,3 +33,26 @@ variable "app_instance_type" {
   default     = "t3.micro"
 }
 
+variable "db_name" {
+  description = "Name of the Tasqly PostgreSQL database"
+  type        = string
+  default     = "tasqly"
+}
+
+variable "db_username" {
+  description = "Master username for the Tasqly database"
+  type        = string
+  default     = "tasqly_admin"
+}
+
+variable "db_password" {
+  description = "Master password for the Tasqly database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
